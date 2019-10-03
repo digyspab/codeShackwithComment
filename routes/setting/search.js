573c346783d46854eaf52fb3582a3f8686244565
Aditya <aditya.kumar@prolitus.com>
2019-10-03 12:47:26 +0530

    search implement

--- routes/setting/search.js
+++ routes/setting/search.js
@@ -6,20 +6,19 @@ const router = express.Router();
 
 router.get('/all_users', (req, res) => {
     let userid = req.params.id;
+    // console.log("userid",userid);
 
-    console.log("userid",userid);
     // let searchuser = "SELECT name, email FROM users WHERE name LIKE '%" + req.query.key + "%' OR email LIKE '%" + req.query.key + "%' ";
-    let searchuser = "SELECT name, email FROM users";
-    console.log('search' + searchuser)
-
+    let searchuser = "SELECT id, email, username, avtar  FROM users";
+    // console.log('search' + searchuser)
+    
     db.query(searchuser, (err, results, fields) => {
-        console.log(results);
 
-       let str = '';
-       for(let i = 0; i < fields; i++) {
-            str = str + fields[i].name;
-            res.end(str);
-       };
+      if (err) {
+          return res.status(500).send(err);
+      } else {
+          res.end(JSON.stringify(results));
+      }
     });
 });
 
